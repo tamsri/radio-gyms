fixed bvh, box, triangle intersection calculation

radio_gyms/engines/ray_tracer/box.py
@@ -8,26 +8,25 @@ from ...utils.constants import MAX_FLT, MIN_FLT
 class Box:
     min_bound: NDArray = None
     max_bound: NDArray = None
-    triangles: List[Triangle] = None
+    triangles: List[Triangle] = []
     child_left: 'Box' = None
     child_right: 'Box' = None
     alone: bool = False
 
     def __init__(self, triangles: List[Triangle]):
         self.triangles = triangles
-        if len(triangles) == 1:
-            alone = True
+        if len(self.triangles) == 1:
+            self.alone = True
         min_x, min_y, min_z = MAX_FLT, MAX_FLT, MAX_FLT
-        max_x, max_y, max_z = MIN_FLT, MIN_FLT, MAX_FLT
+        max_x, max_y, max_z = MIN_FLT, MIN_FLT, MIN_FLT
 
         for triangle in self.triangles:
             x_list = [triangle.pointA[0], triangle.pointB[0], triangle.pointC[0]]
             y_list = [triangle.pointA[1], triangle.pointB[1], triangle.pointC[1]]
             z_list = [triangle.pointA[2], triangle.pointB[2], triangle.pointC[2]]
-            min_x, max_x = min(x_list), max(x_list)
-            min_y, max_y = min(y_list), max(y_list)
-            min_z, max_z = min(z_list), max(z_list)
-
+            min_x, max_x = min(x_list + [min_x]), max(x_list + [max_x])
+            min_y, max_y = min(y_list + [min_y]), max(y_list + [max_y])
+            min_z, max_z = min(z_list + [min_z]), max(z_list + [max_z])
         self.min_bound = np.array([min_x, min_y, min_z])
         self.max_bound = np.array([max_x, max_y, max_z])
 

radio_gyms/engines/ray_tracer/triangle.py
@@ -1,6 +1,9 @@
 import numpy as np
+
 from typing import Tuple
 from numpy.typing import NDArray
+
+from ...utils.calculations import normalize as VecNorm
 from ...utils.constants import EPSILON
 
 
@@ -19,14 +22,12 @@ class Triangle:
         self.edgeA = self.pointB - self.pointA
         self.edgeB = self.pointC - self.pointA
         self.normal = np.cross(self.edgeA, self.edgeB)
-        
-        norm = np.linalg.norm(self.normal)
-        if norm != 0:
-            self.normal = self.normal/norm
+        self.normal = VecNorm(self.normal)
             
-    def isIntersect(self, ray: Tuple[NDArray, NDArray]) -> float:
-        ray_pos = ray[0]
-        ray_dir = ray[1]
+    def is_intersect(self, ray: Tuple[NDArray, NDArray]) -> float:
+        ray_pos: NDArray = ray[0]
+        ray_dir: NDArray = ray[1]
+
         h = np.cross(ray_dir, self.edgeB)
         a = np.dot(self.edgeA, h)
         if -EPSILON < a < EPSILON:

test/test_tracer.py
@@ -7,7 +7,7 @@ from radio_gyms.engines.ray_tracer.triangle import Triangle
 from radio_gyms.engines.ray_tracer.box import Box
 from numpy.typing import NDArray
 
-POZNAN_OBJ_PATH = os.path.join(os.getcwd(), ".." , "assets", "models", "poznan.obj")
+POZNAN_OBJ_PATH = os.path.join(os.getcwd(), "assets", "models", "poznan.obj")
 
 class TestTracer(unittest.TestCase):
 
@@ -17,12 +17,16 @@ class TestTracer(unittest.TestCase):
         c:NDArray = np.array([0,-4,4])
         test_triangle = Triangle(a, b, c)
 
-        ray_pos = np.array([0, 0 ,0])
-        ray_dir = np.array([0, -10, 0])
+        ray_pos = np.array([0, 15 ,0])
+        ray_dir = np.array([0, -1, 0])
         ray = (ray_pos, ray_dir)
-
-        result = test_triangle.isIntersect(ray)
-        self.assertNotEqual(result, -1)
+        result = test_triangle.is_intersect(ray)
+        self.assertGreater(result, 0)
+        ray_pos2 = np.array([0, 15, 0])
+        ray_dir2 = np.array([0, 1, 0])
+        ray2 = (ray_pos2, ray_dir2)
+        result = test_triangle.is_intersect(ray2)
+        self.assertLess(result, 0)
 
     def test_box(self):
         a: NDArray = np.array([5, 0, -3])
@@ -43,10 +47,19 @@ class TestTracer(unittest.TestCase):
         result2 = box.is_intersect(ray2)
         self.assertEqual(result2, False)
 
-    def test_tracer(self):
+    def test_tracer_direct(self):
         tracer = Tracer(POZNAN_OBJ_PATH)
-        tx_pos = (0, 15, 0)
-        rx_pos = (-30, 1.5, 45)
-        result = tracer.traceOutdoor(tx_pos, rx_pos)
-        self.assertEqual(result["direct"], False)
-        self.assertNotEqual(result["knifeEdges"], [])
+        tx_pos = np.array([0, 15, 0])
+        rx_pos = np.array([-30, 1.5, 45])
+        result1 = tracer.trace_outdoor(tx_pos, rx_pos)
+        self.assertEqual(result1["direct"], False)
+        tx_pos2 = np.array([0, 100, 0])
+        rx_pos2 = np.array([0, 101, 0])
+        result2 = tracer.trace_outdoor(tx_pos2, rx_pos2)
+        self.assertEqual(result2["direct"], True)
+
+    def test_tracer_reflect(self):
+        tracer = Tracer(POZNAN_OBJ_PATH)
+        tx_pos = np.array([0, 15, 0])
+        rx_pos = np.array([-30, 1.5, 45])
+        result = tracer.trace_outdoor(tx_pos, rx_pos)